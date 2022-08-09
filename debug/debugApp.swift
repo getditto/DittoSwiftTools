63b74238fefb556931d41ceae10e333ffb01133f
Rae Mac <633012+okdistribute@users.noreply.github.com>
2022-08-09 12:20:11 -0700

    Initial Commit

---
+++ debug/debugApp.swift
@@ -0,0 +1,17 @@
+//
+//  debugApp.swift
+//  debug
+//
+//  Created by Rae McKelvey on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct debugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
