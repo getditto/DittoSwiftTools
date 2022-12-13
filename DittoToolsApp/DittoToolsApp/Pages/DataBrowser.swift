2473891b5a74a93da599adeb58c29de6af7b9897
Walker Erekson <60948031+texasRanger09@users.noreply.github.com>
2022-12-13 12:21:34 -0700

    Update DataBrowser page

--- DittoToolsApp/DittoToolsApp/Pages/DataBrowser.swift
+++ DittoToolsApp/DittoToolsApp/Pages/DataBrowser.swift
@@ -8,27 +8,29 @@
 import SwiftUI
 import DittoSwift
 import Combine
+import DittoDataBrowser
 
-struct DataBrowser: View {
+struct DataBrowserView: View {
    
     var body: some View {
-       List {
-          ForEach(DittoManager.shared.colls) { collection in
-             let internalCollection: Bool = collection.name.starts(with: "__")
-             NavigationLink {
-                CollectionView(name: collection.name)
-             } label: {
-                MenuListItem(title: collection.name, systemImage: internalCollection ? "lock": "envelope", color: internalCollection ? .black : .blue)
-             }
-
-          }
-       }.navigationTitle("Collections")
+       DataBrowser(ditto: DittoManager.shared.ditto!)
+//       List {
+//          ForEach(DittoManager.shared.colls) { collection in
+//             let internalCollection: Bool = collection.name.starts(with: "__")
+//             NavigationLink {
+//                CollectionView(name: collection.name)
+//             } label: {
+//                MenuListItem(title: collection.name, systemImage: internalCollection ? "lock": "envelope", color: internalCollection ? .black : .blue)
+//             }
+//
+//          }
+//       }.navigationTitle("Collections")
     }
 }
 
-struct DataBrowser_Previews: PreviewProvider {
+struct DataBrowserView_Previews: PreviewProvider {
     static var previews: some View {
-        DataBrowser()
+        DataBrowserView()
     }
 }
 
