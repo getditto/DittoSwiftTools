dbe9c8fdfa6b9a5455e541d104ef820576db03ed
okdistribute <633012+okdistribute@users.noreply.github.com>
2023-03-22 12:23:05 -0700

    Use v4 in the swift tools package

--- Package.swift
+++ Package.swift
@@ -30,6 +30,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/getditto/DittoSwiftPackage", from: "4.0.0"),
+        .package(url: "https://github.com/apple/swift-collections", from: "1.0.0")
     ],
     targets: [
         .target(
