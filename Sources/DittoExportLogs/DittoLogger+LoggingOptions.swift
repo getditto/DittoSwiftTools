96fcc685929e80c7abb32728ed97ddf621df6643
rae <633012+okdistribute@users.noreply.github.com>
2023-06-06 15:37:58 -0700

    Update DittoLogger+LoggingOptions.swift

--- Sources/DittoExportLogs/DittoLogger+LoggingOptions.swift
+++ Sources/DittoExportLogs/DittoLogger+LoggingOptions.swift
@@ -26,6 +26,9 @@ public extension DittoLogger {
                 return "info"
             case .debug:
                 return "debug"
+// XXX(rae): Hiding verbose from the UI because users are tempted to use it, 
+// but performance can be impacted by this level and it doesn't add enough
+// extra value for our team over the debug level.
 //            case .verbose:
 //                return "verbose"
             }
