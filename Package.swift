ddf410fc4a9fa9c155610461e1c53fbd54322033
Ben Chatelain <ben@ditto.live>
2024-04-10 16:44:15 -0600

    🔧 Use SSH urls

--- Package.swift
+++ Package.swift
@@ -48,8 +48,8 @@ let package = Package(
     ],
     dependencies: [
         // Ditto.diskUsage was added in 3.0.1
-        .package(url: "https://github.com/getditto/DittoSwiftPackage", from: "4.0.0"),
-        .package(url: "https://github.com/apple/swift-collections", from: "1.0.0")
+        .package(url: "git@github.com:getditto/DittoSwiftPackage.git", from: "4.0.0"),
+        .package(url: "git@github.com:apple/swift-collections.git", from: "1.0.0")
     ],
     targets: [
         .target(
