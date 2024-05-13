1a921bbdba20b9832d533125b244005f13b28278
Brian Plattenburg <5767019+bplattenburg@users.noreply.github.com>
2024-05-13 07:35:16 -0400

    details

--- Sources/DittoPermissionsHealth/BluetoothManager.swift
+++ Sources/DittoPermissionsHealth/BluetoothManager.swift
@@ -60,7 +60,11 @@ public class BluetoothManager: NSObject, ObservableObject {
         case .resetting:
             return "Resetting"
         case .unsupported:
+#if targetEnvironment(simulator)
+            return "Unsupported (Simulator)"
+#else
             return "Unsupported"
+#endif
         case .unauthorized:
             return "Unauthorized"
         case .poweredOff:
