9b8293a63664d416c69968f06229a3615f1005b3
Brian Plattenburg <5767019+bplattenburg@users.noreply.github.com>
2024-05-14 15:55:59 -0400

    WIP

---
+++ Sources/DittoDiskUsage/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Brian Plattenburg on 5/14/24.
+//
+
+import Foundation
+
+struct DittoDiskUsageConstants {
+    static let diskUsageHealthMetricName = "DittoDiskUsage"
+}
---
+++ Sources/DittoDiskUsage/DiskUsageViewModel.swift
@@ -0,0 +1,85 @@
+//
+//  File.swift
+//  
+//
+//  Created by Brian Plattenburg on 5/14/24.
+//
+
+import Foundation
+import Combine
+import DittoSwift
+
+struct DiskUsage: Hashable {
+    let relativePath: String
+    let sizeInBytes: Int
+    let size: String
+}
+
+struct DiskUsageState {
+    let rootPath: String
+    let totalSizeInBytes: Int
+    let totalSize: String
+    let children: [DiskUsage]
+    let lastUpdated: String
+    let error: String?
+}
+
+class DiskUsageViewModel: ObservableObject {
+
+    @Published var diskUsage: DiskUsageState?
+    var cancellable: Cancellable?
+
+    /// Convenience property for Ditto instance.
+    private var ditto: Ditto
+
+    /// Formats file sizes like:
+    /// - 248 bytes
+    /// - 58 KB
+    /// - 4.2 MB
+    private static let byteCountFormatter: ByteCountFormatter = {
+        let formatter = ByteCountFormatter()
+        formatter.countStyle = .file
+        return formatter
+    }()
+
+    /// Formats times like: 12:38:45 PM
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        return formatter
+    }()
+
+    /// Uses `byteCountFormatter` to create a human-readable string.
+    private func formatBytes(bytes: Int) -> String {
+        guard let formattedSize = DiskUsageViewModel.byteCountFormatter.string(for: bytes) else { return "error" }
+        return formattedSize
+    }
+
+    init(ditto: Ditto) {
+        self.ditto = ditto
+        cancellable = ditto.diskUsage
+            .diskUsagePublisher()
+            .map { diskUsage in
+                let children = diskUsage.childItems
+                    .map { (child: DiskUsageItem) in
+                        DiskUsage(
+                            relativePath: child.path,
+                            sizeInBytes: child.sizeInBytes,
+                            size: DiskUsageViewModel.byteCountFormatter.string(for: child.sizeInBytes) ?? "error"
+                        )
+                    }
+                    .sorted { $0.relativePath < $1.relativePath }
+
+                return DiskUsageState(
+                    rootPath: diskUsage.path,
+                    totalSizeInBytes: diskUsage.sizeInBytes,
+                    totalSize: DiskUsageViewModel.byteCountFormatter.string(for: diskUsage.sizeInBytes) ?? "error",
+                    children: children,
+                    lastUpdated: DiskUsageViewModel.dateFormatter.string(from: Date()),
+                    error: nil
+                )
+            }
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.diskUsage, on: self)
+    }
+}
