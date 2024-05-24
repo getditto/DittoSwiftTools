d4291c87ebe775133ff830f889ef4d9d64e36812
Erik Everson <32311723+ErikEverson@users.noreply.github.com>
2024-05-24 07:43:52 -0600

    Updated permissions health for dark mode and multiplatform UI

--- Sources/DittoPermissionsHealth/PermissionsHealth.swift
+++ Sources/DittoPermissionsHealth/PermissionsHealth.swift
@@ -22,49 +22,56 @@ public struct PermissionsHealth: View {
                     VStack(alignment: .leading) {
                         HStack {
                             Text("Bluetooth Permission")
-                                .font(.title)
+                                .font(.title2)
+                                .foregroundColor(.black)
                                 .fontWeight(.bold)
-                            
+
                             Spacer()
                             Image(systemName: checkBluetoothState(state: bluetoothManager.authorizationStatusDescription) ? "checkmark.circle.fill" : "exclamationmark.triangle.fill")
                                 .foregroundColor(checkBluetoothState(state: bluetoothManager.authorizationStatusDescription) ? .green : .gray)
-                                .font(.title)
+                                .font(.title2)
                         }
                         Divider()
                         Text("Permission: \(bluetoothManager.authorizationStatusDescription)")
+                            .foregroundColor(.black)
                         if !checkBluetoothState(state: bluetoothManager.authorizationStatusDescription) {
                             Divider()
                             Button(action: {
                                 openBluetoothSettings()
                             }) {
                                 Text("Authorize Bluetooth")
+// TvOS does its own thing with buttons already
+#if !os(tvOS)
                                     .foregroundColor(.white)
                                     .padding(7)
                                     .background(Color.blue)
                                     .cornerRadius(10)
+#endif
                             }
                         }
                     }
                     .padding()
                     .background(Color.white)
                     .cornerRadius(10)
-                    .shadow(radius: 5) 
+                    .shadow(radius: 5)
                 }
                 .padding()
                 ZStack {
                     VStack(alignment: .leading) {
                         HStack {
                             Text("Bluetooth Status")
-                                .font(.title)
+                                .font(.title2)
                                 .fontWeight(.bold)
-                            
+                                .foregroundColor(.black)
+
                             Spacer()
                             Image(systemName: checkBluetoothState(state: bluetoothManager.managerStateDescription) ? "checkmark.circle.fill" : "exclamationmark.triangle.fill")
                                 .foregroundColor(checkBluetoothState(state: bluetoothManager.managerStateDescription) ? .green : .gray)
-                                .font(.title)
+                                .font(.title2)
                         }
                         Divider()
                         Text("Bluetooth: \(bluetoothManager.managerStateDescription)")
+                            .foregroundColor(.black)
                         if !checkBluetoothState(state: bluetoothManager.managerStateDescription) {
                             Divider()
                             if(bluetoothManager.managerStateDescription == "Unauthorized") {
@@ -74,10 +81,13 @@ public struct PermissionsHealth: View {
                                     openBluetoothSettings()
                                 }) {
                                     Text("Enable Bluetooth")
+// TvOS does its own thing with buttons already
+#if !os(tvOS)
                                         .foregroundColor(.white)
                                         .padding(7)
                                         .background(Color.blue)
                                         .cornerRadius(10)
+#endif
                                 }
                             }
                         }
@@ -92,26 +102,31 @@ public struct PermissionsHealth: View {
                     VStack(alignment: .leading) {
                         HStack {
                             Text("Wi-fi Status")
-                                .font(.title)
+                                .font(.title2)
                                 .fontWeight(.bold)
-                            
+                                .foregroundColor(.black)
+
                             Spacer()
                             Image(systemName: networkManager.isWifiEnabled ? "checkmark.circle.fill" : "exclamationmark.triangle.fill")
                                 .foregroundColor(networkManager.isWifiEnabled ? .green : .gray)
-                                .font(.title)
+                                .font(.title2)
                         }
                         Divider()
                         Text("Wi-Fi: \(networkManager.isWifiEnabled ? "Enabled" : "Disabled")")
+                            .foregroundColor(.black)
                         if !networkManager.isWifiEnabled {
                             Divider()
                             Button(action: {
                                 openWifiSettings()
                             }) {
                                 Text("Enable Wi-fi")
+// TvOS does its own thing with buttons already
+#if !os(tvOS)
                                     .foregroundColor(.white)
                                     .padding(7)
                                     .background(Color.blue)
                                     .cornerRadius(10)
+#endif
                             }
                         }
                     }
