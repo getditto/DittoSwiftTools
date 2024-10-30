9f51925a857a52cd5b141a203795ff686fc9c190
Brian Plattenburg <5767019+bplattenburg@users.noreply.github.com>
2024-10-30 15:09:05 -0400

    Create prerelease branch to support Ditto 4.9.0-rc.2

--- Package.swift
+++ Package.swift
@@ -53,7 +53,7 @@ let package = Package(
             targets: ["DittoAllToolsMenu"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/getditto/DittoSwiftPackage", from: "4.8.0"),
+        .package(url: "https://github.com/getditto/DittoSwiftPackage", branch: "prerelease/4.9.0-rc.2"),
         .package(url: "https://github.com/apple/swift-collections", from: "1.0.0")
     ],
     targets: [
