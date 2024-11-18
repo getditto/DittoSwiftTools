d7378ba5f672718a050a3880e709ee2ed70b6c20
Brian Plattenburg <5767019+bplattenburg@users.noreply.github.com>
2024-11-17 22:23:54 -0500

    doc comments

--- Sources/DittoSyncStatus/DittoSyncStatusHelper.swift
+++ Sources/DittoSyncStatus/DittoSyncStatusHelper.swift
@@ -10,11 +10,19 @@ import DittoSwift
 
 public typealias DittoSyncSubscriptionStatusHandler = (_ result: DittoSyncSubscriptionStatus) -> Void
 
+/// A status that describes whether a set of `DittoSyncSubscription`s is syncing or idle.
+/// This can be combined with an online / offline check to provide an approximation of whether this subscription is up to date
 public enum DittoSyncSubscriptionStatus {
     case idle
     case syncing
 }
 
+/**
+ A helper which provide the sync status of a set of DittoSyncSubscriptions, either idle or syncing.
+ This tells you if this peer is actively receiving data about this subscription from connected peers or idling
+ It can be used to provide an approximation of whether this peer is up to date with other connected peers.
+ It works by creating local store observers for each passed in subscription, then tracking when they fire and comparing against the `idleTimeoutInterval`
+ */
 public class DittoSyncStatusHelper {
     public var idleTimeoutInterval: TimeInterval = 5
 
@@ -33,6 +41,15 @@ public class DittoSyncStatusHelper {
     private var observers: [DittoStoreObserver] = []
     private var lastUpdated: Date = .distantPast
 
+    /**
+     Creates a new` DittoSyncStatusHelper` for a given set of `DittoSyncSubscription`s
+     - Parameters:
+        - ditto: A Ditto instance for which sync status is being checked. Used internally to create `DittoStoreObserver`s tracking each query..
+        - idleTimeoutInterval: How long after the last update is received before this subscription is considered `idle`. Defaults to 5 seconds.
+        - subscriptions: Which subscriptions to include for this status helper. The aggregate status for all of them will be tracked here, such that  it is only considered `idle` if all subscriptions are `idle`.
+        - pollingInterval: How often to provide status updates. Defaults to 0.1 seconds.
+        - handler: A closure called each time the `status` changes.
+     */
     init(ditto: Ditto,
          subscriptions: [DittoSyncSubscription],
          pollingInterval: TimeInterval = 0.1,
