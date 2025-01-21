d121611755a9922cd718aa2b73f2f6ccf2707a0c
Brian Plattenburg <5767019+bplattenburg@users.noreply.github.com>
2025-01-21 10:35:48 -0500

    Revert "Use 4.8.3 now that the iOS 18 crash fix is backported to 4.8.x"
    This reverts commit 827e31b9105e841a2fd3b2079bc7ed2d6f1e6e42.

--- Package.swift
+++ Package.swift
@@ -54,7 +54,7 @@ let package = Package(
             targets: ["DittoAllToolsMenu"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/getditto/DittoSwiftPackage", from: "4.8.3"),
+        .package(url: "https://github.com/getditto/DittoSwiftPackage", from: "4.9.1"),
         .package(url: "https://github.com/apple/swift-collections", from: "1.0.0")
     ],
     targets: [
