d5c7a587de77875af658ff8049f5607235cd67a3
Walker Erekson <walker@ditto.live>
2025-03-18 13:34:53 -0600

    Add support for macOS to DiskUsage and ExportData tools

--- Sources/DittoExportData/ExportData.swift
+++ Sources/DittoExportData/ExportData.swift
@@ -1,4 +1,3 @@
-#if !os(macOS)
 import DittoSwift
 
 #if canImport(SwiftUI)
@@ -9,9 +8,8 @@ import SwiftUI
 import UIKit
 #endif
 
-
 @available(tvOS, unavailable)
-public struct ExportData: UIViewControllerRepresentable {
+public struct ExportData: View {
 
     private let ditto: Ditto
     private let fileManager: FileManager
@@ -21,8 +19,21 @@ public struct ExportData: UIViewControllerRepresentable {
         self.fileManager = fileManager
     }
 
-    public func makeUIViewController(context: Context) -> UIActivityViewController {
+    public var body: some View {
+        #if os(iOS)
+        ExportData_iOS(ditto: ditto, fileManager: fileManager)
+        #endif
+    }
+}
 
+// MARK: - iOS Implementation
+#if os(iOS)
+struct ExportData_iOS: UIViewControllerRepresentable {
+    
+    let ditto: Ditto
+    let fileManager: FileManager
+
+    func makeUIViewController(context: Context) -> UIActivityViewController {
         let zippedURL = zipDittoDirectory()
 
         let activityViewController = UIActivityViewController(activityItems: [zippedURL as Any], applicationActivities: nil)
@@ -32,7 +43,6 @@ public struct ExportData: UIViewControllerRepresentable {
     }
 
     private func zipDittoDirectory() -> URL? {
-
         let destinationURL = fileManager.temporaryDirectory.appendingPathComponent("DittoData.zip")
 
         try? FileManager().removeItem(at: destinationURL)
@@ -51,14 +61,69 @@ public struct ExportData: UIViewControllerRepresentable {
         if let error = nsError {
             assertionFailure("Ditto directory zipping failed. \(error.localizedDescription)")
             return nil
-
         }
 
         return destinationURL
     }
 
-    public func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
         // Do nothing
     }
 }
 #endif
+
+
+// MARK: - macOS Implementation
+#if os(macOS)
+public struct ExportData_macOS {
+    
+    let ditto: Ditto
+    let fileManager: FileManager
+
+    public init(ditto: Ditto, fileManager: FileManager = FileManager.default) {
+        self.ditto = ditto
+        self.fileManager = fileManager
+    }
+
+    public func export() {
+        DispatchQueue.main.async {
+            presentSharingPicker()
+        }
+    }
+
+    private func presentSharingPicker() {
+        guard let zippedURL = zipDittoDirectory() else { return }
+
+        let sharingServicePicker = NSSharingServicePicker(items: [zippedURL])
+        if let window = NSApplication.shared.keyWindow {
+            sharingServicePicker.show(relativeTo: .zero, of: window.contentView!, preferredEdge: .minY)
+        }
+    }
+
+    private func zipDittoDirectory() -> URL? {
+        let destinationURL = fileManager.temporaryDirectory.appendingPathComponent("DittoData.zip")
+
+        try? FileManager().removeItem(at: destinationURL)
+
+        var nsError: NSError?
+
+        NSFileCoordinator().coordinate(readingItemAt: self.ditto.persistenceDirectory,
+                                       options: [.forUploading], error: &nsError) { tempURL in
+            do {
+                try FileManager().moveItem(at: tempURL, to: destinationURL)
+            } catch {
+                assertionFailure("Ditto directory zipping failed.")
+            }
+        }
+
+        if let error = nsError {
+            assertionFailure("Ditto directory zipping failed. \(error.localizedDescription)")
+            return nil
+        }
+
+        return destinationURL
+    }
+}
+#endif
+
+
